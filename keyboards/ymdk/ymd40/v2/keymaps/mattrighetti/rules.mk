a93f6937053ebd0cbb3775768db5e1684f88f2fe
Mattia Righetti <matt95.righetti@gmail.com>
2021-06-05 02:39:43 +0200

    Disabled LEDs and make some keys transparent

---
+++ keyboards/ymdk/ymd40/v2/keymaps/mattrighetti/rules.mk
@@ -0,0 +1,4 @@
+MOUSEKEY_ENABLE    = yes       # Mouse keys
+BACKLIGHT_ENABLE   = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE    = no       # Enable keyboard RGB underglow
+TAP_DANCE_ENABLE   = yes
