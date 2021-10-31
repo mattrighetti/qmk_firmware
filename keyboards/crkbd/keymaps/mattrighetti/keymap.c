e57fa046e7be4a3c364f8608c872c64b78a76b00
Mattia Righetti <matt95.righetti@gmail.com>
2021-10-31 12:09:37 +0100

    Add RGBRST button and move media buttons

--- keyboards/crkbd/keymaps/mattrighetti/keymap.c
+++ keyboards/crkbd/keymaps/mattrighetti/keymap.c
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGBRST ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, XXXXXXX,
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MPLY, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_ENT,    KC_SPC, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -143,14 +143,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case RGBRST:
             if (record->event.pressed) {
-#    ifdef RGBLIGHT_ENABLE
-                eeconfig_update_rgblight_default();
-                rgblight_enable();
-#    elif RGB_MATRIX_ENABLE
-                eeconfig_update_rgb_matrix_default();
-                rgb_matrix_enable();
-#    endif
-                save_rgb_config();
+#               ifdef RGBLIGHT_ENABLE
+                    eeconfig_update_rgblight_default();
+                    rgblight_enable();
+#               elif RGB_MATRIX_ENABLE
+                    eeconfig_update_rgb_matrix_default();
+                    rgb_matrix_enable();
+#               endif
+                    save_rgb_config();
             }
             return false;
 #endif
